b9edfca7119fecb34a165a3f982c3357059d4766
anilamurali <anilamurali98@gmail.com>
2023-09-29 14:59:31 +0530

    Weather search

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
+import Main from './Components/Main';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Main/>
       <Footer/>
     </div>
   );
